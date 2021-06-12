cea56627f3e206dd5e379220cb08ec819b00d253
IcaroCastro <icarocastro05@gmail.com>
2021-06-12 01:12:08 -0300

    M&M Combat Manager // Pequenas alterações e (possível) finalização da def de dano

--- File Class.py
+++ File Class.py
@@ -1,9 +1,9 @@
 class File:
     def __init__(self):
-        self.penal_dmg = 0
         self.circ = 0
         self.incapac = False
         self.conds = list()
+        self.dmgc = list()
         while True:
             try:
                 self.stats = {'Stts': {'STR': int(input('Strength: ')),
@@ -14,21 +14,43 @@ class File:
                                        'DEX': int(input('Dexterity: ')),
                                        'INT': int(input('Intellect: ')),
                                        'PRE': int(input('Presence: '))},
-                              'Defs': {'Dodge': int(input('Dodge: ')),
-                                       'Parry': int(input('Parry: ')),
-                                       'Fortitude': int(input('Fortitude: ')),
-                                       'Toughness': int(input('Toughness: ')),
-                                       'Will': int(input('Will: '))}}
+                              'Defs': {'DODGE': int(input('Dodge: ')),
+                                       'PARRY': int(input('Parry: ')),
+                                       'FORTITUDE': int(input('Fortitude: ')),
+                                       'TOUGHNESS': int(input('Toughness: ')),
+                                       'WILL': int(input('Will: '))}}
             except ValueError:
                 print('Only integers are supported.')
                 continue
-        self.dmgc = list()
 
     def damage(self):
+        penal_dmg = 0
         for _ in self.dmgc:
-            self.penal_dmg += 1
+            penal_dmg += 1
         if 2 in self.dmgc and 'Dazed' not in self.conds:
             self.conds.append('Dazed')
         if 3 in self.dmgc and 'Staggered' not in self.conds:
             self.conds.append('Staggered')
-        
+        while True:
+            try:
+                dmg = int(input('Damage dealt: '))
+                break
+            except ValueError:
+                print('Only integers are supported.')
+                continue
+        while True:
+            res = input('Resistance type [Toughness, Fortitude, Will]: ').upper()
+            if res != 'TOUGHNESS' and res != 'FORTITUDE' and res != 'WILL':
+                print('Only Toughness, Fortitude and Will are valid resistance types.')
+                continue
+            else:
+                try:
+                    dado = int(input('Resistance check result: '))
+                    break
+                except ValueError:
+                    print('Only integers are supported.')
+                    continue
+        condit = (dmg - dado + penal_dmg) // 5
+        self.dmgc.append(condit)
+        if 4 in self.dmgc or self.dmgc.count(3) > 1:
+            self.incapac = True
