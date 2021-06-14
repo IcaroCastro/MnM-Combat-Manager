90ad31382de87538970a40ac851989703f9058da
IcaroCastro <icarocastro05@gmail.com>
2021-06-14 16:26:05 -0300

    M&M Combat Manager // Reformatação da classe de fichas para salvar os dados em um arquivo de texto, se torna mais rápido para adicionar personagens já existentes

--- File_Class.py
+++ File_Class.py
@@ -1,28 +1,66 @@
 class File:
     def __init__(self):
         self.circ = 0
+        self.circ_comm = str()
         self.incapac = False
         self.conds = list()
         self.dmgc = list()
+        self.stats = {'STR': '',
+                              'AGI': '',
+                              'FGT': '',
+                              'AWA': '',
+                              'STA': '',
+                              'DEX': '',
+                              'INT': '',
+                              'PRE': '',
+                              'DODGE': '',
+                              'PARRY': '',
+                              'FORTITUDE': '',
+                              'TOUGHNESS': '',
+                              'WILL': ''}
+        self.name = input('Character Name: ')
+        try:
+            self.getstats(self.name)
+        except FileNotFoundError:
+            self.register(self.name)
+
+    def getstats(self, name):
+        file = open(f'Sheets/{name}.txt', 'r')
+        data = file.read()
+        treat = data.split('\n')
+        file.close()
+        del data, file
+        keys = list()
+        for stat in treat:
+            for key in self.stats.keys():
+                if key in stat:
+                    self.stats[key] = int(stat.split(' ')[1])
+
+    def register(self, name):
+        file = open(f'Sheets/{name}.txt', 'a+')
         while True:
             try:
-                self.stats = {'Stts': {'STR': int(input('Strength: ')),
-                                       'AGI': int(input('Agility: ')),
-                                       'FGT': int(input('Fighting: ')),
-                                       'AWA': int(input('Awareness: ')),
-                                       'STA': int(input('Stamina: ')),
-                                       'DEX': int(input('Dexterity: ')),
-                                       'INT': int(input('Intellect: ')),
-                                       'PRE': int(input('Presence: '))},
-                              'Defs': {'DODGE': int(input('Dodge: ')),
-                                       'PARRY': int(input('Parry: ')),
-                                       'FORTITUDE': int(input('Fortitude: ')),
-                                       'TOUGHNESS': int(input('Toughness: ')),
-                                       'WILL': int(input('Will: '))}}
+                self.stats = {'STR': int(input('Strength: ')),
+                              'AGI': int(input('Agility: ')),
+                              'FGT': int(input('Fighting: ')),
+                              'AWA': int(input('Awareness: ')),
+                              'STA': int(input('Stamina: ')),
+                              'DEX': int(input('Dexterity: ')),
+                              'INT': int(input('Intellect: ')),
+                              'PRE': int(input('Presence: ')),
+                              'DODGE': int(input('Dodge: ')),
+                              'PARRY': int(input('Parry: ')),
+                              'FORTITUDE': int(input('Fortitude: ')),
+                              'TOUGHNESS': int(input('Toughness: ')),
+                              'WILL': int(input('Will: '))}
                 break
             except ValueError:
                 print('Only integers are supported.')
                 continue
+        file.write(f'{name}\n\n')
+        for key in self.stats.keys():
+            file.write(f'{key}: {self.stats[key]}\n')
+        file.close()
 
     def damage(self):
         penal_dmg = 0
@@ -55,3 +93,6 @@ class File:
         self.dmgc.append(condit)
         if 4 in self.dmgc or self.dmgc.count(3) > 1:
             self.incapac = True
+
+a = File()
+print(a.stats)
