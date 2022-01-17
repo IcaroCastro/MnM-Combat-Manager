828c4daffbc545e5e5b4130a3c26323ff7d3217a
IcaroCastro <icarocastro05@gmail.com>
2022-01-17 14:33:52 -0300

    Pausa para almoço // Pixmap concluído

--- SecondaryWindows.py
+++ SecondaryWindows.py
@@ -1,7 +1,9 @@
 import PyQt5.QtWidgets as qtw
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 import json
+from random import randint
 
 
 class CondictWindow(qtw.QMainWindow):
@@ -51,12 +53,64 @@ class CreateCharWindow(qtw.QMainWindow):
         uic.loadUi('GUI/CharWindow.ui', self)
         self.show()
 
+        self.data = dict()
+
+        self.name = self.findChild(qtw.QLineEdit, 'charName')
         self.token = self.findChild(qtw.QLabel, 'charImage')
+        self.stats1 = self.findChild(qtw.QTableWidget, 'statsTable')
+        self.stats2 = self.findChild(qtw.QTableWidget, 'statsTable_2')
+        self.addConButt = self.findChild(qtw.QPushButton, 'addCondit')
+        self.spinBox = self.findChild(qtw.QSpinBox, 'resModSpinBox')
+        self.conditList = self.findChild(qtw.QListWidget, 'conditList')
 
         self.imageButton = self.findChild(qtw.QPushButton, 'findFileButton')
         self.imageButton.clicked.connect(self.getImage)
 
+        self.spinBox.setRange(-20, 0)
+        self.conditList.addItem('-0 to Will/Fort/Tough')
+
+
     def getImage(self):
-        fname = qtw.QFileDialog.getOpenFileName(self, 'Open File', '', 'All Files (*)')
-        self.pixmap = QPixmap(fname[0])
-        self.token.setPixmap(self.pixmap)
+        self.fname = qtw.QFileDialog.getOpenFileName(self, 'Open File', '', 'All Files (*)')
+        pixmap = QPixmap(self.fname[0])
+        pixmap = pixmap.scaled(200, 200, Qt.KeepAspectRatio)
+        self.token.setPixmap(pixmap)
+
+    def save(self):
+        originalImage = open(self.fname[0], 'rb')
+        imageData = originalImage.read()
+        newImage = open(f'Images/{self.name.text()}.png', 'wb+')
+        newImage.write(imageData)
+        originalImage.close()
+        newImage.close()
+        del originalImage, newImage
+
+        statData = list()
+        for x in range(0, 8):
+            statData.append(int(self.stats1.item(x, 0).text()))
+        for y in range(0, 6):
+            statData.append(int(self.stats2.item(y, 0).text()))
+
+        self.data = {
+            'name': self.name.text(),
+            'token': f'Images/{self.name.text()}.png',
+            'stats': {
+                'STR': statData[0],
+                'AGI': statData[1],
+                'FGT': statData[2],
+                'AWA': statData[3],
+                'STA': statData[4],
+                'DEX': statData[5],
+                'INT': statData[6],
+                'PRE': statData[7],
+                'Dodge': statData[8],
+                'Parry': statData[9],
+                'Will': statData[10],
+                'Fortitude': statData[11],
+                'Toughness': statData[12],
+                'Initiative': statData[13]
+            },
+            'conditions': {
+                'resMod':
+            }
+        }
